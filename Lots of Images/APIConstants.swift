693c48a4148129c2294ebf30a5b2fbb18c1e9afd
Vince <vincent.kearney@saltdna.com>
2017-06-11 23:14:35 +0100

    Making a request for images inside a Flickr gallery, boilerplate code in place

---
+++ Lots of Images/APIConstants.swift
@@ -0,0 +1,67 @@
+//
+//  APIConstants.swift
+//  Lots of Images
+//
+//  Created by Vince Kearney on 10/06/2017.
+//  Copyright © 2017 vince. All rights reserved.
+//
+
+import UIKit
+
+class APIConstants : NSObject
+{
+    static let baseURL = "https://api.flickr.com/services/rest/"
+
+    struct Keys {
+        static let method = "method"
+        static let API_Key = "api_key"
+        static let GalleryID = "gallery_id"
+        static let Extras = "extras"
+        static let Format = "format"
+        static let NoJSONCallback = "nojsoncallback"
+    }
+    
+    struct Values {
+        static let API_Key = "7dc182fa62d5ded06cb54094e8e88b99"
+        static let ResponseFormat = "json"
+        static let DisableJSONCallback = "1" // 1 means 'yes'
+        static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
+        static let ColourGalleryId = "72157679219461630"
+    }
+    
+    public static func getGalleryPhotosUrl(withId : String) -> URL
+    {
+        let methodParameters = [
+            APIConstants.Keys.method : APIConstants.Values.GalleryPhotosMethod,
+            APIConstants.Keys.API_Key : APIConstants.Values.API_Key,
+            APIConstants.Keys.GalleryID : withId,
+            APIConstants.Keys.Extras : "url_m",
+            APIConstants.Keys.Format : APIConstants.Values.ResponseFormat,
+            APIConstants.Keys.NoJSONCallback : APIConstants.Values.DisableJSONCallback
+        ]
+        
+        let urlString : String = APIConstants.baseURL + encoding(withParameters: methodParameters as [String : AnyObject])
+        print("This is the urlString -> \(urlString)")
+        return URL(string: urlString)!
+    }
+    
+    private static func encoding(withParameters parameters: [String:AnyObject]) -> String
+    {
+        if parameters.isEmpty
+        {
+            return ""
+        }
+        else
+        {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters
+            {
+                let stringValue = "\(value)"
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+            }
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
+    }
+}
--- Lots of Images/ViewController.swift
+++ Lots of Images/ViewController.swift
@@ -13,7 +13,40 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.getImagesForGallery(withId: APIConstants.Values.ColourGalleryId)
+    }
+    
+    private func getImagesForGallery(withId : String)
+    {
+        let request : URLRequest = URLRequest(url: APIConstants.getGalleryPhotosUrl(withId: withId))
         
+        /// Task
+        let task = URLSession.shared.dataTask(with: request)
+        { (data, response, error) in
+        
+            if error == nil
+            {
+                if let newData = data
+                {
+                    let parsed : [String : AnyObject]!
+                    do {
+                        parsed = try JSONSerialization.jsonObject(with: newData, options: .allowFragments) as! [String : AnyObject]
+                    } catch {
+                        print("Could not parse the data as JSON: \(newData)")
+                        return
+                    }
+                    
+                    print("Total images are -> \(((parsed["photos"] as? [String : AnyObject])?["total"])!)")
+                    let photosDictionary = parsed["photos"] as? [String : AnyObject]
+                    let photoArrays = photosDictionary?["photo"] as? Array<Dictionary<String, AnyObject>> // or [[String : AnyObject]]
+                    
+                    print(photoArrays?[0] ?? "Did not get the array of photos back")
+                }
+            }
+        }
+        
+        task.resume()
+        /// Task
     }
 }
 
